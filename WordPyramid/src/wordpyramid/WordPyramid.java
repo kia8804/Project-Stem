1403294049c2ad91bd648b849328e81bbfb2bcda
kia8804 <mirkamandarikiarash@gmail.com>
2022-04-28 20:24:49 -0400

    Update WordPyramid.java

--- WordPyramid/src/wordpyramid/WordPyramid.java
+++ WordPyramid/src/wordpyramid/WordPyramid.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package wordpyramid;
 
 import java.util.Scanner;
-/**
- *
- * @author guest123
- */
+
 public class WordPyramid {
 
     
